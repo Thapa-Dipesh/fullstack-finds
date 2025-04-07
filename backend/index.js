44c2b4e1fe2ae3443893da00ea0da93f8045d8ca
Thapa-Dipesh <dthapa4682@gmail.com>
2025-04-07 07:41:17 +0545

    create, login, logout user

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 
 // utilities
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 4000;
@@ -19,8 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
