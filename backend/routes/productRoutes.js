36793252b9e8735492b407d23482245c33223fd3
Thapa-Dipesh <dthapa4682@gmail.com>
2025-04-10 20:57:19 +0545

    product management backend

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import formidable from "express-formidable";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+import {
+  addProduct,
+  updateProductDetails,
+  deleteProduct,
+  getProducts,
+  getProductById,
+  getAllProduts,
+  addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(getProducts)
+  .post(authenticate, authorizeAdmin, formidable(), addProduct);
+
+router.route("/allproducts").get(getAllProduts);
+router
+  .route("/:id/reviews")
+  .post(authenticate, authorizeAdmin, checkId, addProductReview);
+
+router.get("/topproducts", fetchTopProducts);
+router.get("/newproduct", fetchNewProducts);
+
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails)
+  .delete(authenticate, authorizeAdmin, deleteProduct);
+
+export default router;
