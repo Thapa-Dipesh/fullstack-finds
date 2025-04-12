ef26558a89af4a4bb0226ecff5f88822b456526c
Thapa-Dipesh <dthapa4682@gmail.com>
2025-04-12 11:56:10 +0545

    admin menu added

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -8,6 +8,7 @@ import {
 import { toast } from "react-toastify";
 import CategoryForm from "../../components/CategoryForm";
 import Modal from "../../components/Modal";
+import AdminMenu from "./AdminMenu";
 
 const CategoryList = () => {
   const { data: categories } = useFetchCategoriesQuery();
@@ -89,7 +90,7 @@ const CategoryList = () => {
 
   return (
     <div className="ml-[10rem] flex flex-col md:flex-row">
-      {/* <AdminMenu /> */}
+      <AdminMenu />
 
       <div className="md:w-3/4 p-3">
         <div className="h-12">Manage Categories</div>
